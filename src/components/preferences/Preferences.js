cd5fb3ee73d67bf7770da8b491266182fcb2784c
Cakeageddon <collinvanleeuwen@gmail.com>
2022-07-09 16:44:54 +0200

    Voorkeursliders werkend gemaakt.

--- src/components/preferences/Preferences.js
+++ src/components/preferences/Preferences.js
@@ -1,17 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Preferences() {
+    const [preferences, setPreferences] = useState({
+        temperature: 50,
+        cloudiness: 0,
+        windspeed: 0,
+    })
+    // const [windspeed, setWindSpeed] = useState('1')
+    // const [temperature, setTemperature] = useState('')
+    // const [cloudiness, setCloudiness] = useState('')
 
     return (
         <>
             <fieldset>
                 <legend>Geef hier jouw voorkeuren aan</legend>
-                <label htmlFor="temperature" className="margin">Temperatuur:</label>
-                <span>Koud<input type="range" id="temperature-slider" name="temperature" min="0" max="100"/>Warm</span><br/>
-                <label htmlFor="cloudiness" className="margin">Bewolking:</label>
-                <span>Geen<input type="range" id="cloudiness-slider" name="cloudiness" min="0" max="100"/>Veel</span><br/>
-                <label htmlFor="windspeed" className="margin">Windkracht:</label>
-                <span>Geen<input type="range" id="windspeed-slider" name="windspeed" min="0" max="100"/>Veel</span>
+
+                <div className="temperature-container">
+                    <label htmlFor="temperature" className="margin">
+                        Temperatuur:
+                    </label>
+                    <br/>
+                    Koud
+                    <input
+                        type="range"
+                        id="temperature-slider"
+                        name="temperature"
+                        min="0"
+                        max="100"
+                        value={preferences.temperature}
+                        onChange={
+                            (e) => setPreferences({
+                                    ...preferences,
+                                    temperature: (e.target.value)
+                                }
+                            )
+                        }
+                    />
+                    Warm {preferences.temperature}
+                </div>
+
+                <div className="cloudiness-container">
+                    <label htmlFor="cloudiness" className="margin">Bewolking %:
+                    </label>
+                    <br/>
+                    Geen
+                    <input
+                        type="range"
+                        id="cloudiness-slider"
+                        name="cloudiness"
+                        min="0"
+                        max="100"
+                        value={preferences.cloudiness}
+                        onChange={
+                            (e) => setPreferences({
+                                    ...preferences,
+                                    cloudiness: (e.target.value)
+                                }
+                            )
+                        }
+                    />
+                    Veel {preferences.cloudiness}
+                </div>
+
+                <div className="windspeed-container">
+                    <label htmlFor="windspeed" className="margin">
+                        Windkracht (Schaal van beaufort):
+                    </label>
+                    <br/>
+                    Geen
+                    <input
+                        type="range"
+                        id="windspeed-slider"
+                        name="windspeed"
+                        min="0"
+                        max="12"
+                        value={preferences.windspeed}
+                        onChange={
+                            (e) => setPreferences({
+                                    ...preferences,
+                                    windspeed: (e.target.value)
+                                }
+                            )
+                        }
+                    />
+                    Veel {preferences.windspeed}
+                </div>
             </fieldset>
 
         </>
